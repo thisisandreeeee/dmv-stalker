5e035671804abbd2b86ec77db30013f0c20c346b
Andre Tan <tanandre93@gmail.com>
2016-08-31 01:19:33 -0700

    add comments to settings file

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-SLACK_CHANNEL = '#dmv'
-URL = 'https://www.dmv.ca.gov/wasapp/foa/findDriveTest.do'
+SLACK_CHANNEL = '#dmv' # this should be the slack channel which you want to send messages to
+URL = 'https://www.dmv.ca.gov/wasapp/foa/findDriveTest.do' # the url for the DMV web form
 LOCATIONS = {
-    'San Mateo': '130',
+    'San Mateo': '130', # the office ID obtained by inspecting the xpath, this is what selenium uses to identify the correct option
     'Redwood City': '109',
     'San Jose': '125',
     'Daly City': '28'
@@ -12,8 +12,9 @@ PROFILE = {
     'mm': '09',
     'dd': '28',
     'yyyy': '1993',
-    'dl_number': 'y4497779',
-    'tel_prefix': '650',
-    'tel_suffix1': '660',
-    'tel_suffix2': '7970'
+    'dl_number': 'your-dl-number-here',
+    'tel_prefix': 'your-area-code-here',
+    'tel_suffix1': 'your-prefix-here',
+    'tel_suffix2': 'your-line-number-here'
+    # format: (area-code) prefix - lineNumber
 }
