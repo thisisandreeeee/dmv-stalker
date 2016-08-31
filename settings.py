1808e566d50050ce5278c5a24b90fd13cb9d43ea
Andre Tan <tanandre93@gmail.com>
2016-08-31 00:57:15 -0700

    create settings file

---
+++ settings.py
@@ -0,0 +1,19 @@
+SLACK_CHANNEL = '#dmv'
+URL = 'https://www.dmv.ca.gov/wasapp/foa/findDriveTest.do'
+LOCATIONS = {
+    'San Mateo': '130',
+    'Redwood City': '109',
+    'San Jose': '125',
+    'Daly City': '28'
+}
+PROFILE = {
+    'first_name': 'ANDRE',
+    'last_name': 'TAN',
+    'mm': '09',
+    'dd': '28',
+    'yyyy': '1993',
+    'dl_number': 'y4497779',
+    'tel_prefix': '650',
+    'tel_suffix1': '660',
+    'tel_suffix2': '7970'
+}
